eba566a642148f58a1dcd08a1b7062e4ac599786
Jim Partan <jpartan@whoi.edu>
2015-04-03 16:02:23 -0400

    Update copy_boot_files.sh with directory argument

--- copy_boot_files.sh
+++ copy_boot_files.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+BOOTDIR=$1
+
 # make the boot directory
-mkdir -p boot
-cp u-boot/MLO boot/	# this must be done first, at least onto SD card
-cp u-boot/u-boot.img boot/
-cp linux/arch/arm/boot/uImage boot/
-cp linux/.config boot/kernel_config_XXXX
-cp gumstix_dev_host/boot-README.txt boot/README.txt
+#mkdir -p ${BOOTDIR}
+
+cp u-boot/MLO ${BOOTDIR}/.	# copying MLO must be done first, at least onto SD card
+cp u-boot/u-boot.img ${BOOTDIR}/.
+cp linux/arch/arm/boot/uImage ${BOOTDIR}/.
+cp linux/.config ${BOOTDIR}/kernel_config_XXXX
+cp gumstix_dev_host/boot-README.txt ${BOOTDIR}/README.txt
 
