17fc93ed68aadb7fcf1f937a44351e25fbdffa7b
Jim Partan <jpartan@whoi.edu>
2015-04-15 15:03:25 -0400

    Updated paths in copy_boot_files.sh

--- copy_boot_files.sh
+++ copy_boot_files.sh
@@ -7,7 +7,9 @@ BOOTDIR=$1
 
 cp u-boot/MLO ${BOOTDIR}/.	# copying MLO must be done first, at least onto SD card
 cp u-boot/u-boot.img ${BOOTDIR}/.
-cp linux/arch/arm/boot/uImage ${BOOTDIR}/.
-cp linux/.config ${BOOTDIR}/kernel_config_XXXX
+#cp linux/arch/arm/boot/uImage ${BOOTDIR}/.
+#cp linux/.config ${BOOTDIR}/kernel_config_3.2.68
+cp ../boot-3.2.68/uImage ${BOOTDIR}/.
+cp ../boot-3.2.68/kernel_config_3.2.68 ${BOOTDIR}/.
 cp gumstix_dev_host/boot-README.txt ${BOOTDIR}/README.txt
 
